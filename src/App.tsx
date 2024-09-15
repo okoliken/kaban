c231d758ec0c66d9532752ce13e45a30439a02bd
okoliken <jahbuchimokoli3@gmail.com>
2024-09-15 18:32:50 +0100

    project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 
+import { Button } from "@/components/ui/button"
+
 function App() {
   return (
     <>
       <h1 className="text-xl font-bold underline">Hello world!</h1>
 
       <div className="pl-3">
-     
+        <Button >Button</Button>
       </div>
     </>
   );
