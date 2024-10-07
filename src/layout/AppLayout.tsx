4b9dafea89b269bc404d32c6dcd55183088e7808
okoliken <jahbuchimokoli3@gmail.com>
2024-10-07 14:41:05 +0100

    figured out a way to handle modals, installed react modal

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -30,7 +30,7 @@ export const AppLayout = (props: React.PropsWithChildren) => {
 
   const modalStyles = {
     overlayClassName:
-      'fixed inset-0 z-50 bg-black bg-opacity-50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+      'fixed inset-0 z-50 bg-black bg-opacity-50',
     className:
       "fixed left-[50%] bg-white dark:bg-[#2B2C37] dark:text-white top-[50%] z-50 grid w-full max-w-[30rem] translate-x-[-50%] translate-y-[-50%] gap-4 p-8 bg-background  shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-6",
   };
